454add2038782c0b86a771447946d0c3a26697e4
pewobox79 <pewobox79@gmail.com>
2022-10-24 15:56:31 +0200

    w5t1 useEffect, useState

--- woche5/src/Components/w5t1/stateManagement/ObjectState.js
+++ woche5/src/Components/w5t1/stateManagement/ObjectState.js
@@ -1,8 +1,9 @@
-import React, {useEffect, useState} from 'react';
+import React, { useEffect, useState } from 'react';
+import { v4 as uuidv4 } from 'uuid';
 
-export default function ObjectState(){
+export default function ObjectState() {
 
-    const [member, setMember]= useState({});
+    const [member, setMember] = useState({});
     const [memberList, setMemberList] = useState([]);
 
     /* function handleChange(event){
@@ -15,47 +16,59 @@ export default function ObjectState(){
 
     } */
 
-    function handleChange({target}){
-        const {name, value} = target;
-        setMember(prevValue => ({ ...prevValue, [name]: value}));
+    useEffect(()=>{
+
+        console.log("hallo effekt")
+
+    },[memberList])
+
+
+    function handleChange({ target }) {
+        const { name, value } = target;
+        setMember(prevValue => ({ ...prevValue, [name]: value, uuid: uuidv4() }));
     }
 
     console.log(member);
 
-    function addMember(e){
+    function addMember(e) {
         //schaltet natürliches verhalten des form buttons aus....
         e.preventDefault();
-        setMemberList(prev=>[...prev, member])
-    
+        setMemberList(prev => [...prev, member])
+
     }
 
     console.log(memberList);
 
-    
-    const memberShip = memberList.map((member, index)=>{
-            return <div key={index}>{member.firstname}</div>
-        })
-       
-
-    
+    function removeMember(event) {
+        const selectedUuid = event.target.id;
+        setMemberList(prev => prev.filter(member => {
+            return member.uuid !== selectedUuid
+        }))
 
 
-    
- 
-return(
-    <div>
-    <h1>Membership</h1>
 
-    <form>
-        <input type="text" name="firstname" placeholder="Firstname" onChange={handleChange}/>
-        <input type="text" name="lastname" placeholder="Lastname" onChange={handleChange}/>
-        <input type="text" name="country" placeholder="Country" onChange={handleChange}/>
-        <button onClick={addMember}>Sign Up</button>
-    </form>
+    }
 
-    <div>
-        {memberShip ? <h1>no member</h1>: memberShip}
-    </div>
-    </div>
-)
+    const memberShip = memberList.map((member) => {
+        return <div key={member.uuid} id={member.uuid} onClick={removeMember}>{member.firstname} {member.lastname} from {member.country}</div>
+    });
+
+
+
+    return (
+        <div>
+            <h1>Membership</h1>
+
+            <form>
+                <input type="text" name="firstname" placeholder="Firstname" onChange={handleChange} />
+                <input type="text" name="lastname" placeholder="Lastname" onChange={handleChange} />
+                <input type="text" name="country" placeholder="Country" onChange={handleChange} />
+                <button onClick={addMember}>Sign Up</button>
+            </form>
+
+            <div>
+                {memberShip.length < 1 ? <h1>no member</h1> : memberShip}
+            </div>
+        </div>
+    )
 }
